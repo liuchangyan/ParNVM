c4fc602f8cf3038c232eeab29b2480d84cd6440c
XuChen <xuchen@u.nus.edu>
2018-06-29 14:01:10 +0800

    libc and pmem first try

--- pnvm/src/main.rs
+++ pnvm/src/main.rs
@@ -1,10 +1,70 @@
-extern crate pnvm_lib;
+extern crate libc;
+//extern crate nvml_sys;
 
+use libc::*;
+use std::ffi::CString;
+use std::fs::File;
+use std::mem::*;
 
-fn main() {
-    println!("Refer to pnvm_lib for library source!");
+#[link(name = "pmem")]
+extern "C" {
+    pub fn pmem_check_version(major_required: c_uint, minor_required: c_uint) -> *const c_char;
+    pub fn pmem_deep_drain(addr: *const c_void, len: usize) -> c_int;
+    pub fn pmem_deep_flush(addr: *const c_void, len: usize);
+    pub fn pmem_deep_persist(addr: *const c_void, len: usize) -> c_int;
+    pub fn pmem_drain();
+    pub fn pmem_errormsg() -> *const c_char;
+    pub fn pmem_flush(addr: *const c_void, len: usize);
+    pub fn pmem_has_hw_drain() -> c_int;
+    pub fn pmem_is_pmem(addr: *const c_void, len: usize) -> c_int;
+    pub fn pmem_map_file(
+        path: *const c_char,
+        len: usize,
+        flags: c_int,
+        mode: mode_t,
+        mapped_lenp: *mut usize,
+        is_pmemp: *mut c_int,
+    ) -> *mut c_void;
+    pub fn pmem_msync(addr: *const c_void, len: usize) -> c_int;
+    pub fn pmem_persist(addr: *const c_void, len: usize);
+    pub fn pmem_unmap(addr: *mut c_void, len: usize) -> c_int;
+
 }
 
+pub const PMEM_FILE_CREATE: c_int = 1;
+pub const PMEM_FILE_EXCL: c_int = 2;
+pub const PMEM_FILE_SPARSE: c_int = 4;
+pub const PMEM_FILE_TMPFILE: c_int = 8;
 
+const PMEM_LEN: usize = 4096;
 
+fn main() -> std::io::Result<()> {
+    let mut mapped_len: usize = unsafe { uninitialized() };
+    let mapped_lenp = &mut mapped_len as *mut _;
+    let mut is_pmem: i32 = unsafe { uninitialized() };
+    let is_pmemp = &mut is_pmem as *mut _;
+    let filepath = CString::new("myfile").unwrap();
+    let filepath_ptr = filepath.as_ptr();
+    let res = unsafe {
+        pmem_map_file(
+            filepath_ptr,
+            PMEM_LEN,
+            PMEM_FILE_CREATE,
+            0666,
+            mapped_lenp,
+            is_pmemp,
+        )
+    };
 
+    if res.is_null() {
+        println!("Null pointer...");
+    } else if is_pmem == 0 {
+        println!("No pesistent memory..");
+    } else if is_pmem == 1 {
+        println!("WOWOWYOOHOH!");
+    } else {
+        panic!("pmem_map_file returns crap");
+    }
+
+    Ok(())
+}
--- pnvm_lib/src/txn.rs
+++ pnvm_lib/src/txn.rs
@@ -1,8 +1,8 @@
-use std::sync::Arc;    
-use tobj::{TTag, TObject, ObjectId};
 use std::collections::HashMap;
+use std::sync::Arc;
+use tobj::{ObjectId, TObject, TTag};
 
-#[derive(PartialEq,Copy, Clone, Debug)]
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Tid(u32);
 
 impl Tid {
@@ -11,175 +11,165 @@ impl Tid {
     }
 }
 
-
 pub struct Transaction<T>
-where T: Clone
-   {
-        tid_:   Tid,
-        state_: TxState,
-        deps_:  HashMap<ObjectId, TTag<T>>
+where
+    T: Clone,
+{
+    tid_: Tid,
+    state_: TxState,
+    deps_: HashMap<ObjectId, TTag<T>>,
+}
+
+impl<T> Transaction<T>
+where
+    T: Clone,
+{
+    pub fn new(tid_: Tid) -> Transaction<T> {
+        Transaction {
+            tid_,
+            state_: TxState::EMBRYO,
+            deps_: HashMap::new(),
+        }
     }
 
-    impl<T> Transaction<T>
-    where T: Clone
-    {
-        pub fn new(tid_: Tid) -> Transaction<T> {
-            Transaction {
-                tid_,
-                state_: TxState::EMBRYO,
-                deps_ : HashMap::new(),
+    pub fn commit_id(&self) -> Tid {
+        self.tid_
+    }
+
+    pub fn try_commit(&mut self) -> bool {
+        println!("Tx[{:?}] is commiting", self.tid_);
+        self.state_ = TxState::COMMITTED;
+
+        //Stage 1: lock [TODO: Bounded lock or try_lock syntax]
+        if !self.lock() {
+            return self.abort(AbortReason::FailedLocking);
+        }
+
+        //Stage 2: Check
+        if !self.check() {
+            return self.abort(AbortReason::FailedLocking);
+        }
+
+        //Stage 3: Commit
+        self.commit();
+        true
+    }
+
+    pub fn abort(&mut self, _: AbortReason) -> bool {
+        println!("Tx[{:?}] is aborting.", self.tid_);
+        self.state_ = TxState::ABORTED;
+        self.clean_up();
+        false
+    }
+
+    pub fn read(&mut self, tobj: &TObject<T>) -> T {
+        let tobj = Arc::clone(tobj);
+
+        let _tobj = tobj.read().unwrap();
+        let id = _tobj.get_id();
+        let tag = self.retrieve_tag(id, Arc::clone(&tobj));
+        tag.add_version(_tobj.get_version());
+        if tag.has_write() {
+            tag.write_value()
+        } else {
+            _tobj.get_data()
+        }
+    }
+
+    pub fn write(&mut self, tobj: &TObject<T>, val: T) {
+        let tobj = Arc::clone(tobj);
+
+        let _tobj = tobj.write().unwrap();
+        let tag = self.retrieve_tag(_tobj.get_id(), Arc::clone(&tobj));
+        tag.write(val);
+    }
+
+    pub fn lock(&mut self) -> bool {
+        let mut locks: Vec<&TTag<T>> = Vec::new();
+
+        for tag in self.deps_.values() {
+            if !tag.has_write() {
+                continue;
             }
-        }
-
-        pub fn commit_id(&self) -> Tid {
-            self.tid_
-        }
-        
-
-        pub fn try_commit(&mut self) -> bool {
-            println!("Tx[{:?}] is commiting", self.tid_);
-            self.state_ = TxState::COMMITTED;
-
-            //Stage 1: lock [TODO: Bounded lock or try_lock syntax]
-            if !self.lock() {
-                return self.abort(AbortReason::FailedLocking);
-            }
-
-            //Stage 2: Check 
-            if !self.check() {
-                return self.abort(AbortReason::FailedLocking);
-            }
-            
-            //Stage 3: Commit 
-            self.commit(); 
-            true
-        }
-
-        pub fn abort(&mut self, _: AbortReason) -> bool {
-            println!("Tx[{:?}] is aborting.", self.tid_);
-            self.state_ = TxState::ABORTED;
-            self.clean_up();
-            false 
-        }
-
-
-
-        pub fn read(&mut self, tobj: &TObject<T>) -> T {
-            let tobj = Arc::clone(tobj);
-
-            let _tobj = tobj.read().unwrap();
-            let id = _tobj.get_id(); 
-            let tag  = self.retrieve_tag(id, Arc::clone(&tobj));
-            tag.add_version(_tobj.get_version());
-            if tag.has_write() {
-                 tag.write_value()
-            } else {
-                 _tobj.get_data()
-            }
-        }
-
-        pub fn write(&mut self, tobj: &TObject<T>, val : T) {
-            let tobj = Arc::clone(tobj);
-
-            let _tobj = tobj.write().unwrap();
-            let tag = self.retrieve_tag(_tobj.get_id(), Arc::clone(&tobj));
-            tag.write(val);
-        }
-
-        pub fn lock(&mut self) -> bool {
-            let mut locks :Vec<&TTag<T>>= Vec::new();
-
-            for tag in self.deps_.values() {
-                if !tag.has_write() {
-                    continue;
-                }
-                let mut _tobj = tag.tobj_ref_.write().unwrap();
-                if !_tobj.lock(self.commit_id()) {
-                    while let Some(_tag) = locks.pop() {
-                       let mut _tobj = _tag.tobj_ref_.write().unwrap();
-                       _tobj.unlock();
-                    }
-                    println!("{:#?} failed to locked!", tag);
-                    return false;
-                } else {
-                    locks.push(tag);
-                }
-                println!("{:#?} locked!", tag);
-            }
-
-            true
-        }
-
-        fn check(&mut self) -> bool {
-            for tag in self.deps_.values() {
-                if !tag.has_read() {
-                    continue;
-                }
-
-                let _tobj = tag.tobj_ref_.read().unwrap();
-                if !_tobj.check(&tag.vers_) {
-                    return false;
-                }
-            }
-            true
-        }
-
-        fn commit(&mut self) -> bool { 
-            for tag in self.deps_.values() {
-                if tag.has_write() {
-                    let mut _tobj = tag.tobj_ref_.write().unwrap();
-                    _tobj.install(tag.write_value(), self.commit_id());
-                }
-            }
-
-            self.clean_up();
-            true
-        }
-
-        fn clean_up(&mut self) {
-            for tag in self.deps_.values() {
-                if tag.has_write() {
-                    let mut _tobj = tag.tobj_ref_.write().unwrap();
+            let mut _tobj = tag.tobj_ref_.write().unwrap();
+            if !_tobj.lock(self.commit_id()) {
+                while let Some(_tag) = locks.pop() {
+                    let mut _tobj = _tag.tobj_ref_.write().unwrap();
                     _tobj.unlock();
                 }
+                println!("{:#?} failed to locked!", tag);
+                return false;
+            } else {
+                locks.push(tag);
+            }
+            println!("{:#?} locked!", tag);
+        }
+
+        true
+    }
+
+    fn check(&mut self) -> bool {
+        for tag in self.deps_.values() {
+            if !tag.has_read() {
+                continue;
+            }
+
+            let _tobj = tag.tobj_ref_.read().unwrap();
+            if !_tobj.check(&tag.vers_) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn commit(&mut self) -> bool {
+        for tag in self.deps_.values() {
+            if tag.has_write() {
+                let mut _tobj = tag.tobj_ref_.write().unwrap();
+                _tobj.install(tag.write_value(), self.commit_id());
             }
         }
 
-        pub fn retrieve_tag(&mut self, id: ObjectId, tobj_ref: TObject<T> ) -> &mut TTag<T> {
-            self.deps_.entry(id)
-                .or_insert(TTag::new(id, tobj_ref))
-        }
-
-
-       /*Non Transaction Functions*/
-        pub fn notrans_read(tobj: &TObject<T>) -> T {
-            let tobj = Arc::clone(tobj);
-            let _tobj = tobj.read().unwrap();
-            _tobj.raw_read()
-        }
-
-        pub fn notrans_lock(tobj: &TObject<T>, tid :Tid) -> bool {
-            let tobj = Arc::clone(tobj);
-            let mut _tobj = tobj.write().unwrap();
-             _tobj.lock(tid)
-        }
-
-
+        self.clean_up();
+        true
     }
 
-    pub enum TxState {
-        COMMITTED,
-        EMBRYO,
-        ACTIVE,
-        ABORTED,
+    fn clean_up(&mut self) {
+        for tag in self.deps_.values() {
+            if tag.has_write() {
+                let mut _tobj = tag.tobj_ref_.write().unwrap();
+                _tobj.unlock();
+            }
+        }
     }
 
-
-    pub enum AbortReason {
-        Error,
-        User,
-        FailedLocking,
+    pub fn retrieve_tag(&mut self, id: ObjectId, tobj_ref: TObject<T>) -> &mut TTag<T> {
+        self.deps_.entry(id).or_insert(TTag::new(id, tobj_ref))
     }
 
+    /*Non Transaction Functions*/
+    pub fn notrans_read(tobj: &TObject<T>) -> T {
+        let tobj = Arc::clone(tobj);
+        let _tobj = tobj.read().unwrap();
+        _tobj.raw_read()
+    }
 
+    pub fn notrans_lock(tobj: &TObject<T>, tid: Tid) -> bool {
+        let tobj = Arc::clone(tobj);
+        let mut _tobj = tobj.write().unwrap();
+        _tobj.lock(tid)
+    }
+}
 
+pub enum TxState {
+    COMMITTED,
+    EMBRYO,
+    ACTIVE,
+    ABORTED,
+}
+
+pub enum AbortReason {
+    Error,
+    User,
+    FailedLocking,
+}
